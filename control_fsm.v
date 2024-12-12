3ac5ec765348eff9902bab2181596fd399223d8b
Matin Dehghanipor <matindehghanipor010@gmail.com>
2024-12-12 11:08:10 +0330

    reset fixed

--- control_fsm.v
+++ control_fsm.v
@@ -8,9 +8,9 @@ module control_fsm (entry_sensor, exit_sensor, vacant_parking,
     
     input entry_sensor, exit_sensor, CLK, RESET;
 	input [1:0] vacant_parking;
-	output door_open_signal, full_signal;
-	output reg [2:0] capacity;
-	output reg [1:0] best_location;
+	output reg door_open_signal, full_signal;
+	output [2:0] capacity;
+	output [1:0] best_location;
 	output reg [3:0] parkings;
 
 	reg [15:0] timer;
@@ -18,48 +18,41 @@ module control_fsm (entry_sensor, exit_sensor, vacant_parking,
     reg [1:0] state;
     parameter IDLE = 2'b00 , DOOR_OPEN = 2'b01 , FULL = 2'b10 , ASSIGN_PARKING = 2'b11;
 
-    reg full, empty;
+    wire full, empty;
 
-    assign full = parkings[0] & parkings[1] & parkings[2] & parkings[3];
-	assign empty = ~(parkings[0] | parkings[1] | parkings[2] | parkings[3]);
+    assign full = parkings[0] & parkings[1] & parkings[2] & parkings[3] & ~RESET;
+	assign empty = ~(parkings[0] | parkings[1] | parkings[2] | parkings[3]) & ~RESET;
 
-    assign capacity[2] = empty;
-    assign capacity[1] = (~parkings[0] & ~parkings[1] & parkings[2]) |
+    assign capacity[2] = empty & ~RESET;
+    assign capacity[1] = ((~parkings[0] & ~parkings[1] & parkings[2]) |
                         (~parkings[1] & ~parkings[2] & parkings[3]) |
                         (parkings[0] & ~parkings[1] & ~parkings[3]) |
                         (parkings[1] & ~parkings[2] & ~parkings[3]) |
                         (~parkings[0] & parkings[1] & ~parkings[3]) |
-                        (~parkings[0] & parkings[1] & ~parkings[2]);
+                        (~parkings[0] & parkings[1] & ~parkings[2])) & ~RESET;
 
-    assign capacity[0] = (parkings[1] ^ parkings[3]) | 
-                        (parkings[0] ^ parkings[2]);
+    assign capacity[0] = ((parkings[1] ^ parkings[3]) | 
+                        (parkings[0] ^ parkings[2])) & ~RESET;
 
     // The case in which parkings is equal to 1111 is considered don't care
-    assign best_location[1] = parkings[0] & parkings[1];
-    assign best_location[0] = ((~parkings[1]) & parkings[0]) |
-                                (parkings[2] & parkings[0]);
-
-    reg inp;
-	
-	assign inp = {entry_sensor, exit_sensor};    
-		                      
+    assign best_location[1] = parkings[0] & parkings[1] & ~RESET;
+    assign best_location[0] = (((~parkings[1]) & parkings[0]) |
+                                (parkings[2] & parkings[0])) & ~RESET;
+   		                      
     always @ (posedge CLK or posedge RESET) begin
         if (RESET) begin
-            state = 2'b00; // Initialize to IDLE
+            state = IDLE; // Initialize to IDLE
             parkings = 4'b0000;
-            capacity = 2'b00;
-            best_location = 2'b00;
-            full = 1'b0;
-            empty = 1'b0;
         end
 
         case (state)
             IDLE : begin
 				timer = 16'b0000000000000000;
                 if (entery_sensor & ~exit_sensor)
-                    if (full)
+                    if (full) begin
                         state = FULL;
 						full_signal = 1'b1;
+					end
                     else begin
                         state = DOOR_OPEN;
 						door_open_signal = 1'b1;
@@ -77,7 +70,6 @@ module control_fsm (entry_sensor, exit_sensor, vacant_parking,
 					state = IDLE;
 					door_open_signal = 1'b0;
 				end
-
 			 end
 
             FULL : begin 
@@ -100,7 +92,7 @@ module control_fsm (entry_sensor, exit_sensor, vacant_parking,
 					parkings[3] = parkings[3] & (~vacant_parking[0] | ~vacant_parking[1]);
 					parkings[2] = parkings[2] & (~vacant_parking[0] | vacant_parking[1]);
 					parkings[1] = parkings[1] & (vacant_parking[0] | ~vacant_parking[1]);
-					parkings[0] = parkings[0] & (vacant_parking[0] | vacant_parking[1])
+					parkings[0] = parkings[0] & (vacant_parking[0] | vacant_parking[1]);
 				end
 			end
 
